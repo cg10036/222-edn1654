8ecc8c98fa3fcf0f0aba8e8dc3765e5493380f7e
Lee Soo Hyuk <30382481+cg10036@users.noreply.github.com>
2022-09-15 14:10:12 +0900

    Update 05_practice_2.ino

--- 05_practice_2/05_practice_2.ino
+++ 05_practice_2/05_practice_2.ino
@@ -2,17 +2,17 @@
 
 void setup() {
   pinMode(PIN_LED, OUTPUT);
-  digitalWrite(PIN_LED, true);
+  digitalWrite(PIN_LED, false);
   delay(1000);
 }
 
 void loop() {
   for (int i = 0; i < 5; i++) {
-    digitalWrite(PIN_LED, false);
-    delay(100);
     digitalWrite(PIN_LED, true);
     delay(100);
+    digitalWrite(PIN_LED, false);
+    delay(100);
   }
-  digitalWrite(PIN_LED, false);
+  digitalWrite(PIN_LED, true);
   while (true);
 }
